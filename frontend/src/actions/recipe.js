9ea26602407620a173dd9666283c867ace72ed10
jasontoups <me@jasontoups.com>
2024-03-06 15:31:55 -0800

    setup the recipe actions, and received a recipe by id in the fetchRecipeSuccess

--- frontend/src/actions/recipe.js
+++ frontend/src/actions/recipe.js
@@ -1,3 +1,45 @@
 /* TODO: create recipe fetch actions, creators, and constants
   API: use /api/recipe/:id as a get request to fetch the recipe info
 */
+
+export const FETCH_RECIPE_REQUEST = "FETCH_RECIPE_REQUEST"
+export const FETCH_RECIPE_SUCCESS = "FETCH_RECIPE_SUCCESS"
+export const FETCH_RECIPE_FAILURE = "FETCH_RECIPE_FAILURE"
+
+const fetchRecipeRequest = () => {
+  return {
+    type: FETCH_RECIPE_REQUEST,
+  }
+}
+
+const fetchRecipeSuccess = (recipe) => {
+  console.log("fetchRecipeSuccess: ", recipe)
+  return {
+    type: FETCH_RECIPE_SUCCESS,
+    payload: recipe,
+  }
+}
+
+const fetchRecipeFailure = (error) => {
+  return {
+    type: FETCH_RECIPE_FAILURE,
+    payload: error,
+  }
+}
+
+export const fetchRecipe = (id) => {
+  return async (dispatch) => {
+    console.log("fetchRecipe: ", id)
+    dispatch(fetchRecipeRequest())
+    try {
+      const response = await fetch(`/api/recipe/${id}`)
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      const recipe = await response.json()
+      dispatch(fetchRecipeSuccess(recipe))
+    } catch (error) {
+      dispatch(fetchRecipeFailure(error.toString()))
+    }
+  }
+}
