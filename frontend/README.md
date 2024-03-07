50b63b3fd908244f49fe8b290cc0cc06dfe9eef7
jasontoups <me@jasontoups.com>
2024-03-06 18:07:28 -0800

    updates reducers to store a recipe in the Redux Store & updates readme

--- frontend/README.md
+++ frontend/README.md
@@ -1,6 +1,6 @@
 # frontend
 
-The back end is being hosted on http://localhost:3000/
+The front end is being hosted on http://localhost:3000/
 
 ## intro
 
@@ -10,6 +10,6 @@ welcome to the frontend portion of the take home. feel free to edit any part of 
 
 ## TODO
 
-1. fix the search function
-1. add a recipe component to display the response
-1. add routing so that you can land on a recipe page or reload and have the same search
+- [ ] fix the search function
+- [ ] add a recipe component to display the response
+- [ ] add routing so that you can land on a recipe page or reload and have the same search
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,16 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 import thunkMiddleware from "redux-thunk"
-import { createStore, applyMiddleware } from "redux"
+import { createStore, applyMiddleware, compose } from "redux"
 import { hot } from "react-hot-loader"
 import Home from "./Containers/Home"
 import reducers from "./reducers"
 
-const store = createStore(reducers, applyMiddleware(thunkMiddleware))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+)
 
 const WrappedHome = () => (
   <Provider store={store}>
