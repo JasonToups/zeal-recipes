7aa901c7ad311c8990fc0abcda4627f5f9c9b60d
jasontoups <me@jasontoups.com>
2024-03-06 18:07:28 -0800

    updates reducers to store a recipe in the Redux Store & updates readme

--- frontend/src/reducers/recipe.js
+++ frontend/src/reducers/recipe.js
@@ -1,3 +1,36 @@
-/*
-  TODO: Create reducer and state updates here for recipe
-*/
+import {
+  FETCH_RECIPE_REQUEST,
+  FETCH_RECIPE_SUCCESS,
+  FETCH_RECIPE_FAILURE,
+} from "../actions"
+
+const initialState = {
+  recipe: null,
+  isLoadingRecipe: false,
+  error: null,
+}
+
+const recipeFetching = (state) => {
+  return { ...state, isLoadingRecipe: true }
+}
+
+const recipeFetched = (state, payload) => {
+  return { ...state, isLoadingRecipe: false, recipe: payload }
+}
+
+const recipeFailed = (state, payload) => {
+  return { ...state, isLoadingRecipe: false, error: payload }
+}
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    case FETCH_RECIPE_REQUEST:
+      return recipeFetching()
+    case FETCH_RECIPE_SUCCESS:
+      return recipeFetched(state, payload)
+    case FETCH_RECIPE_FAILURE:
+      return recipeFailed(state, payload)
+    default:
+      return state
+  }
+}
